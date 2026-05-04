073ad016cf5c062a9dee5abb257890978c0008da
cadier <ma3wen2bin1@gmail.com>
2026-05-04 13:57:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,15 +4,14 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1. 移除可能冲突的旧插件源 (比如你原来的 helloworld)
+sed -i '/helloworld/d' feeds.conf.default
+sed -i '/passwall/d' feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 2. 添加 ImmortalWrt 官方软件源（包含最新版 HomeProxy 和 Sing-box）
+# 即使你使用的是官方 OpenWrt 源码，也可以通过这种方式拉取 ImmortalWrt 的插件
+echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy.git' >>feeds.conf.default
+
+# 3. (可选) 添加其他常用的依赖源，确保编译不报错
+# echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
