513598224fc4bbf951af0713151dea527f311538
Xghng999 <1369492@qq.com>
2026-06-25 23:55:52 +0800

    Update diy-22-03.sh

--- diy-22-03.sh
+++ diy-22-03.sh
@@ -6,5 +6,5 @@
 
 # 3. (可选) 添加其他常用的依赖源，确保编译不报错
 # echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
-# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package.git' feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/Xghng999/small-package.git' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package.git' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/Xghng999/small-package.git' feeds.conf.default
