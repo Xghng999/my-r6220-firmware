c4955ff98cfd5f2b09d148f27a4e360cebfff44c
Xghng999 <1369492@qq.com>
2026-06-25 15:55:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,12 +6,12 @@
 #
 
 # 1. 移除可能冲突的旧插件源 (比如你原来的 helloworld)
-sed -i '/helloworld/d' feeds.conf.default
-sed -i '/passwall/d' feeds.conf.default
+# sed -i '/helloworld/d' feeds.conf.default
+# sed -i '/passwall/d' feeds.conf.default
 
 # 2. 添加 ImmortalWrt 官方软件源（包含最新版 HomeProxy 和 Sing-box）
 # 即使你使用的是官方 OpenWrt 源码，也可以通过这种方式拉取 ImmortalWrt 的插件
-echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy.git' >>feeds.conf.default
+# echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy.git' >>feeds.conf.default
 
 # 3. (可选) 添加其他常用的依赖源，确保编译不报错
-# echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
+echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
